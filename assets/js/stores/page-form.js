66c49d8f32964ba6950939cb13083be7fa33d550
Mikael Carlsson <mickec79@gmail.com>
2016-01-21 00:12:01 +0100

    deactivate page form store for now

--- assets/js/stores/page-form.js
+++ assets/js/stores/page-form.js
@@ -1,14 +1,5 @@
-// require('dotenv').load();
-console.log('env', process.env);
-
 function PageForm() {
     riot.observable(this);
-
-    this.send = (data) => {
-        console.log('form data', data);
-    }
-
-    this.on('FORM_APPLICATION', this.send);
 }
 
 module.exports = PageForm;
