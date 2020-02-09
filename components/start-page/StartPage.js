8290c64972134c0d1184b7c6e947e3390cdf65ae
Mikael Carlsson <mikael.carlsson@getflowbox.com>
2020-02-09 23:20:44 +0100

    Render Strangells component on start page

--- components/start-page/StartPage.js
+++ components/start-page/StartPage.js
@@ -4,11 +4,13 @@ import Guides from './Guides'
 import Hero from './Hero'
 import Location from './Location'
 import Projects from './Projects'
+import Strangells from './Strangells'
 
 const StartPage = ({ language }) => {
   return (
     <main>
       <Hero />
+      {language === 'se' && <Strangells />}
       <Projects language={language} />
       <Guides language={language} />
       <Location />
