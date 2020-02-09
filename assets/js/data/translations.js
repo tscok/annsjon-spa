2dfd3afeb953022f0801f67452fbcafcebdad34f
Mikael Carlsson <mikael.carlsson@getflowbox.com>
2020-02-09 22:02:10 +0100

    Replace html characters with ascii

--- assets/js/data/translations.js
+++ assets/js/data/translations.js
@@ -1,275 +1,304 @@
 const translations = {
     // Title
-    'site_title': {
+    site_title: {
         se: 'Ånnsjöns fågelstation',
-        en: 'Lake Ånnsjön Bird Observatory'
+        en: 'Lake Ånnsjön Bird Observatory',
     },
 
     // Misc
-    'language': {
+    language: {
         se: 'English',
-        en: 'Svenska'
+        en: 'Svenska',
     },
-    'credits': {
+    credits: {
         se: 'Design och utveckling av',
-        en: 'Design and development by'
+        en: 'Design and development by',
     },
     'read-more': {
-        se: 'Läs&nbsp;mer',
-        en: 'Read&nbsp;more'
+        se: 'Läs\u00a0mer',
+        en: 'Read\u00a0more',
     },
     'thank-you': {
         se: 'Tack',
-        en: 'Thank you'
+        en: 'Thank you',
     },
-    'yes': {
+    yes: {
         se: 'Ja',
-        en: 'Yes'
+        en: 'Yes',
     },
-    'no': {
+    no: {
         se: 'Nej',
-        en: 'No'
+        en: 'No',
     },
-    'select': {
+    select: {
         se: 'Välj',
-        en: 'Select'
+        en: 'Select',
     },
     'first-name': {
         se: 'Förnamn',
-        en: 'First name'
+        en: 'First name',
     },
     'last-name': {
         se: 'Efternamn',
-        en: 'Last name'
+        en: 'Last name',
     },
-    'email': {
+    email: {
         se: 'Epost',
-        en: 'Email'
+        en: 'Email',
     },
-    'phone': {
+    phone: {
         se: 'Telefon',
-        en: 'Phone'
+        en: 'Phone',
     },
 
     // Hero
-    'hero_body': {
+    hero_body: {
         se: 'En ideell förening som arbetar för ökad kunskap om fågellivet i västra Jämtland',
-        en: 'A non-profit organisation working for a better understanding about the birdlife in western Jämtland&nbsp;County in Sweden'
+        en:
+            'A non-profit organisation working for a better understanding about the birdlife in western Jämtland\u00a0County in Sweden',
     },
 
     // Annual Meeting
     'annual-meeting_header': {
         se: 'Årsmöte',
-        en: 'Annual General Meeting'
+        en: 'Annual General Meeting',
     },
     'annual-meeting_title': {
         se: 'Kallelse till årsmöte',
-        en: 'Notice of Annual General Meeting'
+        en: 'Notice of Annual General Meeting',
     },
     'annual-meeting_body': {
-        se: 'Lördagen den 1 juni, kl 13.00, anordnar styrelsen i Föreningen Ånnsjöns Fågelstation årsmöte i Handöl. Medlemmar i föreningen och allmänheten välkomnas att delta.',
-        en: 'The Board of Lake Ånnsjön Bird Observatory invites members and the public community to our Annual General Meeting in Handöl, June 1st 2019 at 1 pm.'
+        se:
+            'Lördagen den 1 juni, kl 13.00, anordnar styrelsen i Föreningen Ånnsjöns Fågelstation årsmöte i Handöl. Medlemmar i föreningen och allmänheten välkomnas att delta.',
+        en:
+            'The Board of Lake Ånnsjön Bird Observatory invites members and the public community to our Annual General Meeting in Handöl, June 1st 2019 at 1 pm.',
     },
 
     // Open House
-    'openhouse_header': {
+    openhouse_header: {
         se: 'Öppet Hus',
-        en: 'Open House'
+        en: 'Open House',
     },
-    'openhouse_title': {
+    openhouse_title: {
         se: 'Ånnsjödagen, 1 juni 2019',
-        en: 'Lake Ånnsjön Day, June 1st 2019'
+        en: 'Lake Ånnsjön Day, June 1st 2019',
     },
-    'openhouse_body': {
-        se: 'Ta med dina vänner till Ånnsjön när fågellivet är som mest sprudlande. Vi hjälper till med artbestämningen vid Ånnsjöns fågeltorn och gömslen under morgontimmarna, bjuder på bildvisning under kvällen och guidar er till en dubbelbeckasinlek vid midnatt.',
-        en: 'Bring your friends to Lake Ånnsjön when the birdlife is peaking. During the early hours we&#39;ll help you identify species at the observation towers by Lake Ånnsjön. During this day we arrange various activities and invites you to a guided tour of a Great Snipe lek at midnight!'
+    openhouse_body: {
+        se:
+            'Ta med dina vänner till Ånnsjön när fågellivet är som mest sprudlande. Vi hjälper till med artbestämningen vid Ånnsjöns fågeltorn och gömslen under morgontimmarna, bjuder på bildvisning under kvällen och guidar er till en dubbelbeckasinlek vid midnatt.',
+        en:
+            'Bring your friends to Lake Ånnsjön when the birdlife is peaking. During the early hours we&#39;ll help you identify species at the observation towers by Lake Ånnsjön. During this day we arrange various activities and invites you to a guided tour of a Great Snipe lek at midnight!',
     },
 
     // Volunteer
-    'volunteer_header': {
+    volunteer_header: {
         se: 'Medarbetare',
-        en: 'Volunteer'
+        en: 'Volunteer',
     },
-    'volunteer_title': {
+    volunteer_title: {
         se: 'Jobba med oss i sommar!',
-        en: 'Work with us this summmer!'
+        en: 'Work with us this summmer!',
     },
-    'volunteer_body': {
-        se: 'Inför säsongen 2016 söker vi vana fältornitologer till våra invent&shy;eringar, som väl känner till fjäll- och våtmarksarterna till utseende och läte. Till ringmärkningen söker vi erfarna ringmärkare och assistenter. Vi&nbsp;erbjuder fri logi och en oslagbar upplevelse i Ånnsjöfjällen.',
-        en: 'For the season 2016 we are looking for experienced field ornithologists that can determine mountain and wetland species by look and sound. For the ringing we are looking for experienced ringers and assistants. We&nbsp;offer free accommodation for volunteers and an unique visit to Lake Ånnsjön.'
+    volunteer_body: {
+        se:
+            'Inför säsongen 2016 söker vi vana fältornitologer till våra invent\u00aderingar, som väl känner till fjäll- och våtmarksarterna till utseende och läte. Till ringmärkningen söker vi erfarna ringmärkare och assistenter. Vi\u00a0erbjuder fri logi och en oslagbar upplevelse i Ånnsjöfjällen.',
+        en:
+            'For the season 2016 we are looking for experienced field ornithologists that can determine mountain and wetland species by look and sound. For the ringing we are looking for experienced ringers and assistants. We\u00a0offer free accommodation for volunteers and an unique visit to Lake Ånnsjön.',
     },
     'volunteer_button-apply': {
         se: 'Anmäl dig här',
-        en: 'Apply here'
+        en: 'Apply here',
     },
 
     // Projects
-    'projects_header': {
+    projects_header: {
         se: 'Några av våra projekt',
-        en: 'Some of our projects'
+        en: 'Some of our projects',
     },
     'projects_title-great-snipe': {
         se: 'Dubbelbeckasin',
-        en: 'Great Snipe'
+        en: 'Great Snipe',
     },
     'projects_body-great-snipe': {
-        se: 'Tillsammans med forskare från Lunds universitet studerar vi dubbel&shy;beckasinens flyttningar.',
-        en: 'Together with scientists from Lund University we study migratory movements of the Great Snipe.'
+        se:
+            'Tillsammans med forskare från Lunds universitet studerar vi dubbel\u00adbeckasinens flyttningar.',
+        en:
+            'Together with scientists from Lund University we study migratory movements of the Great Snipe.',
     },
     'projects_title-census': {
         se: 'Inventering',
-        en: 'Census project'
+        en: 'Census project',
     },
     'projects_body-census': {
-        se: 'Vi inventerar regelbundet Ånnsjön och omkring&shy;liggande myrar samt kalfjäll i området Ånnsjön-Storlien.',
-        en: 'Every spring we conduct census projects on Lake Ånnsjön and the surrounding mires and mountains.'
+        se:
+            'Vi inventerar regelbundet Ånnsjön och omkring\u00adliggande myrar samt kalfjäll i området Ånnsjön-Storlien.',
+        en:
+            'Every spring we conduct census projects on Lake Ånnsjön and the surrounding mires and mountains.',
     },
     'projects_title-ringing': {
         se: 'Ringmärkning',
-        en: 'Ringing'
+        en: 'Ringing',
     },
     'projects_body-ringing': {
-        se: 'Varje år bedriver vi standardiserad ring&shy;märkning av tättingar i Handöl och Enans delta.',
-        en: 'We annually ring between 2000-4000 passerines in the area of Handöl and in the Enan river delta.'
+        se:
+            'Varje år bedriver vi standardiserad ring\u00admärkning av tättingar i Handöl och Enans delta.',
+        en:
+            'We annually ring between 2000-4000 passerines in the area of Handöl and in the Enan river delta.',
     },
-    'projects_button': {
+    projects_button: {
         se: 'Se alla projekt',
-        en: 'See all projects'
+        en: 'See all projects',
     },
 
     // Guides
-    'guides_header': {
+    guides_header: {
         se: 'Vår skådarguide',
-        en: 'Our watcher\'s guide'
+        en: "Our watcher's guide",
     },
     'guides_body-handol': {
-        se: 'Längs älven Handölans lopp genom Handöl sträcker sig ett lummigt, löv&shy;skogs&shy;dominerat område med ett rikt fågelliv. Här hittar man trädgårds&shy;sångare, svarthätta…',
-        en: 'Along the river Handölan, through the village Handöl, there\'s a green, deciduous forest with a rich birdlife. Common species here are Garden Warbler, Blackcap, Icterine…'
+        se:
+            'Längs älven Handölans lopp genom Handöl sträcker sig ett lummigt, löv\u00adskogs\u00addominerat område med ett rikt fågelliv. Här hittar man trädgårds\u00adsångare, svarthätta…',
+        en:
+            "Along the river Handölan, through the village Handöl, there's a green, deciduous forest with a rich birdlife. Common species here are Garden Warbler, Blackcap, Icterine…",
     },
     'guides_body-hogasen': {
-        se: 'Dubbelbeckasinerna anländer till Ånnsjö&shy;fjällen omkring 15 maj, och börjar spela omedelbart. Spelet pågår intensivt någon vecka in i juni. Därefter är de flesta honor…',
-        en: 'The Greate snipe arrives to the mountains surrounding Lake Ånnsjön in mid May. They begin playing immediately and does so very intensively until mid June. After that most of…'
+        se:
+            'Dubbelbeckasinerna anländer till Ånnsjö\u00adfjällen omkring 15 maj, och börjar spela omedelbart. Spelet pågår intensivt någon vecka in i juni. Därefter är de flesta honor…',
+        en:
+            'The Greate snipe arrives to the mountains surrounding Lake Ånnsjön in mid May. They begin playing immediately and does so very intensively until mid June. After that most of…',
     },
     'guides_body-storlien': {
-        se: 'Området mellan Vindarnas tempel och Åhlénstugan brukar kunna bjuda på lappsparv, blåhake, fjällabb och kärrsnäppa. På Skurdals&shy;höjden finns även en välkänd…',
-        en: 'The area between the Temple of Winds and the Åhléns cabin usually offers Lapland Bunting, Bluethroat, Long-tailed Skua and Dunlin. There\'s also a well known Great Snipe lek…'
+        se:
+            'Området mellan Vindarnas tempel och Åhlénstugan brukar kunna bjuda på lappsparv, blåhake, fjällabb och kärrsnäppa. På Skurdals\u00adhöjden finns även en välkänd…',
+        en:
+            "The area between the Temple of Winds and the Åhléns cabin usually offers Lapland Bunting, Bluethroat, Long-tailed Skua and Dunlin. There's also a well known Great Snipe lek…",
     },
     'guides_body-storulvan': {
-        se: 'Här kan man se ripor av bägge arterna, blåhake och på högre höjd fjällpipare och snösparv. Både kungsörn och jaktfalk ses ibland vid Storulvån, men fjällvråk och sten…',
-        en: 'Among the more common species are Willow Grouse, Ptarmigan, Bluethroat and at higher altitude Dotterel and Snow Bunting. Golden Eagle and Gyr Falcon are rare, while Merlin…'
+        se:
+            'Här kan man se ripor av bägge arterna, blåhake och på högre höjd fjällpipare och snösparv. Både kungsörn och jaktfalk ses ibland vid Storulvån, men fjällvråk och sten…',
+        en:
+            'Among the more common species are Willow Grouse, Ptarmigan, Bluethroat and at higher altitude Dotterel and Snow Bunting. Golden Eagle and Gyr Falcon are rare, while Merlin…',
     },
     'guides_body-ann': {
-        se: 'På myrarna vid sjön är småspov, ljungpipare, grönbena, rödbena och gulärla karaktärsfåglar. Storspov, gluttsnäppa och enkel&shy;beckasin förekommer också…',
-        en: 'On the mires by Lake Ånnsjön typical species are Whimbrel, Golden Plover, Wood Sandpiper, Redshank and Yellow Wagtail. Other common species include Curlew, Greenshank…'
+        se:
+            'På myrarna vid sjön är småspov, ljungpipare, grönbena, rödbena och gulärla karaktärsfåglar. Storspov, gluttsnäppa och enkel\u00adbeckasin förekommer också…',
+        en:
+            'On the mires by Lake Ånnsjön typical species are Whimbrel, Golden Plover, Wood Sandpiper, Redshank and Yellow Wagtail. Other common species include Curlew, Greenshank…',
     },
-    'guides_button': {
+    guides_button: {
         se: 'Till skådarguiden',
-        en: 'See the guide'
+        en: 'See the guide',
     },
 
     // Volunteer
-    'volunteer_gender': {
+    volunteer_gender: {
         se: 'Kön',
-        en: 'Gender'
+        en: 'Gender',
     },
     'volunteer_gender-f': {
         se: 'Kvinna',
-        en: 'Female'
+        en: 'Female',
     },
     'volunteer_gender-m': {
         se: 'Man',
-        en: 'Male'
+        en: 'Male',
     },
-    'volunteer_birthyear': {
+    volunteer_birthyear: {
         se: 'Födelseår',
-        en: 'Birthyear'
+        en: 'Birthyear',
     },
-    'volunteer_about': {
+    volunteer_about: {
         se: 'Presentation',
-        en: 'Presentation'
+        en: 'Presentation',
     },
     'volunteer_about-hint': {
         se: 'Berätta om dig själv och tidigare erfarenheter med fåglar.',
-        en: 'Tell us about yourself and any previous experience with birds.'
+        en: 'Tell us about yourself and any previous experience with birds.',
     },
     'volunteer_arrival-date': {
         se: 'Datum för ankomst',
-        en: 'Arrival date'
+        en: 'Arrival date',
     },
     'volunteer_departure-date': {
         se: 'Datum för avfärd',
-        en: 'Departure date'
+        en: 'Departure date',
     },
     'volunteer_invalid-date': {
         se: 'Felaktigt datum',
-        en: 'Invalid date'
+        en: 'Invalid date',
     },
     'volunteer_dates-misplaced': {
         se: 'Ankomstdatum är senare än eller samma som avfärdsdatum.',
-        en: 'Arrival date is later than or same date as departure.'
+        en: 'Arrival date is later than or same date as departure.',
     },
-    'volunteer_timeframe': {
+    volunteer_timeframe: {
         se: 'Tidsram',
-        en: 'Timeframe'
+        en: 'Timeframe',
     },
     'volunteer_timeframe-hint': {
         se: 'Ange ungefär när du skulle vilja jobba hos oss och hur länge.',
-        en: 'Estimate when you would like to volunteer and for how long.'
+        en: 'Estimate when you would like to volunteer and for how long.',
     },
-    'volunteer_nationality': {
+    volunteer_nationality: {
         se: 'Nationalitet',
-        en: 'Nationality'
+        en: 'Nationality',
     },
-    'volunteer_car': {
+    volunteer_car: {
         se: 'Medför bil',
-        en: 'Arrives by car'
+        en: 'Arrives by car',
     },
     'volunteer_driving-license': {
         se: 'Körkort',
-        en: 'Driving license'
+        en: 'Driving license',
     },
-    'volunteer_mandatory': {
+    volunteer_mandatory: {
         se: 'Fält markerade med asterisk (<strong>*</strong>) är obligatoriska.',
-        en: 'Fields marked with asterisk (<strong>*</strong>) are mandatory.'
+        en: 'Fields marked with asterisk (<strong>*</strong>) are mandatory.',
     },
-    'volunteer_errors': {
+    volunteer_errors: {
         se: 'Vänligen korrigera eventuella fel för att skicka formuläret.',
-        en: 'Please correct any errors to submit the form.'
+        en: 'Please correct any errors to submit the form.',
     },
-    'volunteer_undelivered': {
-        se: '<strong>Formuläret skickades inte.</strong> Din ansökan kunde inte skickas på grund av tekniska problem. Vänligen skicka din ansökan till <a href="mailto:volunteer@annsjon.org">volunteer@annsjon.org</a> eller försök igen senare. Vi beklagar besväret!',
-        en: '<strong>Form submission failed.</strong> Your application could not be sent due to technical problems. Please consider applying to <a href="mailto:volunteer@annsjon.org">volunteer@annsjon.org</a> or try again later. We are sorry for the inconvenience!'
+    volunteer_undelivered: {
+        se:
+            '<strong>Formuläret skickades inte.</strong> Din ansökan kunde inte skickas på grund av tekniska problem. Vänligen skicka din ansökan till <a href="mailto:volunteer@annsjon.org">volunteer@annsjon.org</a> eller försök igen senare. Vi beklagar besväret!',
+        en:
+            '<strong>Form submission failed.</strong> Your application could not be sent due to technical problems. Please consider applying to <a href="mailto:volunteer@annsjon.org">volunteer@annsjon.org</a> or try again later. We are sorry for the inconvenience!',
     },
-    'volunteer_submit': {
+    volunteer_submit: {
         se: 'Skicka',
-        en: 'Submit'
+        en: 'Submit',
     },
-    'volunteer_submitting': {
+    volunteer_submitting: {
         se: 'Skickar…',
-        en: 'Submitting…'
+        en: 'Submitting…',
     },
-    'volunteer_confirmation': {
-        se: 'Din ansökan har skickats.<br>Vi återkommer till dig så snart vi har behandlat ärendet.',
-        en: 'Your application has been sent.<br>We will process it and get back to you as soon as possible.'
+    volunteer_confirmation: {
+        se:
+            'Din ansökan har skickats.<br>Vi återkommer till dig så snart vi har behandlat ärendet.',
+        en:
+            'Your application has been sent.<br>We will process it and get back to you as soon as possible.',
     },
 
     // Map
-    'map_header': {
+    map_header: {
         se: 'Här finns vi',
-        en: 'We are here'
+        en: 'We are here',
     },
-    'map_title': {
+    map_title: {
         se: 'Var håller vi till?',
-        en: 'Where we are'
+        en: 'Where we are',
     },
-    'map_body': {
-        se: 'Fågelstationen ligger i den lilla byn Handöl i västra Jämtland, inte långt ifrån norska gränsen. Hit tar man sig lättast med tåg, för vägbeskrivning se vår ',
-        en: 'The Observatory is situated in the tiny village of Handöl in Jämtland County, not far from the Norwegian border. The best way to get here is by train, for directions see our '
+    map_body: {
+        se:
+            'Fågelstationen ligger i den lilla byn Handöl i västra Jämtland, inte långt ifrån norska gränsen. Hit tar man sig lättast med tåg, för vägbeskrivning se vår ',
+        en:
+            'The Observatory is situated in the tiny village of Handöl in Jämtland County, not far from the Norwegian border. The best way to get here is by train, for directions see our ',
     },
-    'map_link': {
+    map_link: {
         se: 'Utforska platsen med ',
-        en: 'Explore the area with '
+        en: 'Explore the area with ',
     },
-};
+}
 
 export default translations
