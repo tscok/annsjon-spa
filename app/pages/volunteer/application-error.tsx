bed62c882196355e8d4b3c47f8b0352c4d01bed9
Mikael Carlsson <mickec79@gmail.com>
2024-03-19 17:20:19 +0100

    Removes copy to clipboard feature as it only works with HTTPS

--- app/pages/volunteer/application-error.tsx
+++ app/pages/volunteer/application-error.tsx
@@ -1,5 +1,3 @@
-import { useRef } from 'react'
-import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
 import { useDictionary } from 'app/i18n/use-dictionary'
 import { interpolate } from 'app/utils/interpolate'
@@ -10,14 +8,6 @@ import { A } from 'app/ui/text/a'
 
 export const ApplicationError = ({ formData }: { formData: string }) => {
   const t = useDictionary('form')
-  const inputRef = useRef<HTMLTextAreaElement>(null)
-
-  const handleCopy = () => {
-    if (!inputRef.current) return
-    inputRef.current.select()
-    const contents = inputRef.current.innerHTML
-    navigator.clipboard.writeText(contents)
-  }
 
   return (
     <>
@@ -29,8 +19,7 @@ export const ApplicationError = ({ formData }: { formData: string }) => {
       </P>
       <Stack spacing={2}>
         <H5>{t('error.formData')}</H5>
-        <TextArea inputRef={inputRef} defaultValue={formData} readOnly />
-        <Button onClick={handleCopy}>{t('error.copyData')}</Button>
+        <TextArea defaultValue={formData} />
       </Stack>
     </>
   )
