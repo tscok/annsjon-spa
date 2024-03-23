d6a073b32f360b75fd2257e0c67d6d519ca610df
Mikael Carlsson <mickec79@gmail.com>
2024-03-23 18:43:15 +0100

    Check response.status on submit

--- app/form/provider.tsx
+++ app/form/provider.tsx
@@ -10,10 +10,14 @@ export const FormProvider = ({ children }: PropsWithChildren) => {
   const onSubmit = async (data: FormData) => {
     const options = { body: data, method: 'post' }
     try {
-      update({ loading: true })
+      update({ ...defaultState, loading: true })
       const response = await fetch('/service.php', options)
-      const status = (await response.json()) as string | false
-      update({ loading: false, success: Boolean(status), data })
+      const responseText = (await response.text()) as string | false
+      if (response.status === 200 && !!responseText) {
+        update({ loading: false, success: true, data })
+      } else {
+        update({ loading: false, error: true, data })
+      }
     } catch (e) {
       update({ loading: false, error: true, data })
     }
