09fa84d28cfc0f055e0887e5258c89639e2c1167
Mikael Carlsson <mickec79@gmail.com>
2024-08-21 15:37:09 +0200

    create date-provider

--- app/pages/volunteer/providers.tsx
+++ app/pages/volunteer/providers.tsx
@@ -1,23 +1,12 @@
-import dayjs from 'dayjs'
-import 'dayjs/locale/sv'
 import { Outlet } from 'react-router-dom'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { FormProvider } from 'app/form/provider'
+import { DateProvider } from './date-provider'
 import { formState } from './form-state'
-import { useLanguage } from 'app/language/use-language'
 
-export const Providers = () => {
-  const { language } = useLanguage()
-
-  return (
-    <FormProvider initialState={formState}>
-      <LocalizationProvider
-        adapterLocale={dayjs.locale(language.locale)}
-        dateAdapter={AdapterDayjs}
-      >
-        <Outlet />
-      </LocalizationProvider>
-    </FormProvider>
-  )
-}
+export const Providers = () => (
+  <FormProvider initialState={formState}>
+    <DateProvider>
+      <Outlet />
+    </DateProvider>
+  </FormProvider>
+)
